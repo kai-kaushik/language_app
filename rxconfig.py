d3e1c6ebb65105c47c561855338d2c6f912f97b5
Kumara-Kaushik <kumarakaushik@gmail.com>
2023-07-23 18:39:15 +0900

    revert to Dev mode

--- rxconfig.py
+++ rxconfig.py
@@ -9,7 +9,11 @@ class LanguageappConfig(rx.Config):
 
 config = LanguageappConfig(
     app_name="language_app",
-    api_url="0.0.0.0:8000",
-    bun_path="/app/.bun/bin/bun",
+    # api_url="0.0.0.0:8000",
+    # bun_path="/app/.bun/bin/bun",
+    # db_url="sqlite:///reflex.db",
+    bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///reflex.db",
+    env=rx.Env.DEV,
+    port=3000,
 )
